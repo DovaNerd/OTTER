5d6ed833775f9e29a98ee3113ca2d79a64546251
Harry <harrison.smith1@ontariotechu.net>
2020-09-25 16:43:31 -0400

    Tut Week 2 (done)

--- samples/INFR-1350U/Week3-Starter/src/Shader.cpp
+++ samples/INFR-1350U/Week3-Starter/src/Shader.cpp
@@ -47,6 +47,12 @@ bool Shader::LoadShaderPart(const char* source, GLenum type)
 		glDeleteShader(handle);
 		handle = 0;
 	}
+	switch (type) {
+	case GL_VERTEX_SHADER: _vs = handle; break;
+	case GL_FRAGMENT_SHADER: _fs = handle; break;
+	default: LOG_WARN("Not implemented"); break;
+	}
+	return status != GL_FALSE;
 }
 
 bool Shader::LoadShaderPartFromFile(const char* path, GLenum type) {
